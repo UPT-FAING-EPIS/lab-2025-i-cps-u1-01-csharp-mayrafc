d10914cae3db24eaab0df6b74702bfe061508858
mayrafc <mc2021072620@virtual.upt.pe>
2025-04-08 13:31:41 -0500

    test1

--- Bank/Bank.Domain.Tests/BanckAccountTests.cs
+++ Bank/Bank.Domain.Tests/BanckAccountTests.cs
@@ -14,27 +14,11 @@ namespace Bank.Domain.Tests
             double debitAmount = 4.55;
             double expected = 7.44;
             BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
-
             // Act
             account.Debit(debitAmount);
-
             // Assert
             double actual = account.Balance;
             Assert.AreEqual(expected, actual, 0.001, "Account not debited correctly");
-        }
-
-        [TestMethod]
-        public void Credit_WithValidAmount_UpdatesBalance()
-        {
-            double initialBalance = 11.99;
-            double creditAmount = 5.00;
-            double expected = 16.99;
-            BankAccount account = new BankAccount("Mr. Bryan Walton", initialBalance);
-
-            account.Credit(creditAmount);
-
-            double actual = account.Balance;
-            Assert.AreEqual(expected, actual, 0.001, "Account not credited correctly");
         }
     }
 }
